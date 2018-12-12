280f5256330c23df66675f02a50e907f9fa386d8
magdalenaatzl <1810653965@stud.fh-kufstein.ac.at>
2018-12-12 11:28:23 +0100

    Aufgabe 5

--- src/at/ac/fh_kufstein/uebung_05/Classes/Aufruf.java
+++ src/at/ac/fh_kufstein/uebung_05/Classes/Aufruf.java
@@ -4,10 +4,10 @@ public class Aufruf {
 
     public static void main(String[] args) {
 
-        Auto bmw = new Auto (4, "silber", 220, 5, 4);
-        Auto audi = new Auto (4, "schwarz", 180, 5, 8);
+        Auto bmw = new Auto ((short)4, "silber", (short)220, (short)5, (short)4);
+        Auto audi = new Auto ((short)4, "schwarz", (short)180, (short)5, (short)8);
+        Wasserfahrzeug titanic = new Wasserfahrzeug((short)0, "blau", (short)51000, (short)0, 10.54, (short)3, (short)100000);
 
-        System.out.println(bmw.getReifen());
 
 
 
